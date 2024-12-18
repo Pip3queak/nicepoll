ec097ed273f41a5300da0a6004b11cbad2c212a2
pip3queak <weini.chong@gmail.com>
2024-12-18 21:22:36 +0800

    default

---
+++ page.tsx
@@ -0,0 +1,68 @@
+//lucid
+import { Lucid, Blockfrost } from "@lucid-evolution/lucid";
+import { Wallet } from "@/types/cardano";
+
+//lucid
+
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  //lucid
+  const NETWORK = process.env.NEXT_PUBLIC_CARDANO_NETWORK as Network;
+  const BF_URL = `${process.env.NEXT_PUBLIC_BF_URL}`;
+  const BF_PID = `${process.env.NEXT_PUBLIC_BF_PID}`;
+  //lucid
+
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <span className={title()}>Make&nbsp;</span>
+        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <br />
+        <span className={title()}>
+          websites regardless of your design experience.
+        </span>
+        <div className={subtitle({ class: "mt-4" })}>
+          Beautiful, fast and modern React UI library.
+        </div>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+          href={siteConfig.links.docs}
+        >
+          Documentation
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Snippet hideCopyButton hideSymbol variant="bordered">
+          <span>
+            Get started by editing <Code color="primary">app/page.tsx</Code>
+          </span>
+        </Snippet>
+      </div>
+    </section>
+  );
+}
